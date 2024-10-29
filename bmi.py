c9cb44367d8565f0ef925fa95bd531f0256a6171
j4vent4n <javentan16.23@ichat.sp.edu.sg>
2024-10-29 15:53:48 +0800

    with classification

--- bmi.py
+++ bmi.py
@@ -1,21 +1,21 @@
-def calculate_bmi(height,weight):
+def calculate_bmi(height, weight):
     print("Height=" + str(height))
     print("Weight=" + str(weight))
 
+    bmi = weight / (height * height)
 
-    bmi = weight/ (height * height )
+    print("BMI=" + str(bmi))
 
-    print("BMI="+str(bmi))
+    if bmi < 18.5:
+        classification = "Under Weight"
+        return bmi, classification, -1
+    elif 18.5 <= bmi <= 25.0:
+        classification = "Normal Weight"
+        return bmi, classification, 0
+    else:
+        classification = "Over Weight"
+        return bmi, classification, 1
 
-    if bmi < 18.5 :
-        print("Weight Classification: Under Weight")
-    if 18.5 <= bmi <= 25.0:
-        print("Weight Classification: Normal Weight")
-    if bmi > 25:
-        print("Weight Classification: Over Weight")
 
- 
-
-
-calculate_bmi(weight=40,height=1.73)
-
+bmi_value, classification, return_value = calculate_bmi(weight=40, height=1.73)
+print(f"BMI Value: {bmi_value}, Classification: {classification}, Return Value: {return_value}")
